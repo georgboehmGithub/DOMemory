2c7c6df96dad08e1e8428cd20bc22d1a835a9593
georgboehmGithub <georgboehmcc@hotmail.com>
2023-10-05 21:42:22 +0200

    reformat App.js file

--- App.js
+++ App.js
@@ -1,26 +1,28 @@
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Home from './screens/Home';
-import Help from './screens/Help';
-import Stats from './screens/Stats';
-import Settings from './screens/Settings';
-import SetOverview from './screens/SetOverview';
-import React, { useEffect, useState } from 'react';
+import Home from "./screens/Home";
+import Help from "./screens/Help";
+import Stats from "./screens/Stats";
+import Settings from "./screens/Settings";
+import SetOverview from "./screens/SetOverview";
+import React, { useEffect, useState } from "react";
 import { initializeSetDatabase, initializeCardDatabase } from "./database";
-import Session from './screens/Session';
-import { SafeAreaView, Platform} from 'react-native';
+import Session from "./screens/Session";
+import { SafeAreaView, Platform } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
-function HomeStack({isSetDatabaseInitialized}) {
+function HomeStack({ isSetDatabaseInitialized }) {
   return (
-    <Stack.Navigator screenOptions={{
-      headerShown: false
-    }}>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
       <Stack.Screen
         name="HomeStack"
         component={Home}
@@ -32,10 +34,9 @@ function HomeStack({isSetDatabaseInitialized}) {
   );
 }
 
-
 export default function App() {
-
-  const [isSetDatabaseInitialized, setIsSetDatabaseInitialized] = useState(false);
+  const [isSetDatabaseInitialized, setIsSetDatabaseInitialized] =
+    useState(false);
 
   useEffect(() => {
     // Initialize the set database if it has not yet been created
@@ -43,42 +44,63 @@ export default function App() {
       setIsSetDatabaseInitialized(true);
     });
     // Initialize the card database if it has not yet been created
-    initializeCardDatabase(() => {
-    })
+    initializeCardDatabase(() => {});
   }, []);
- 
+
   return (
     <NavigationContainer>
-      <SafeAreaView style={{flex: 1, paddingTop: Platform.OS === 'android' ? 35 : 0}}>
-      <Tab.Navigator screenOptions={{
-    headerShown: false
-  }}>
-      <Tab.Screen options={{
-      tabBarLabel: 'Home',
-      tabBarIcon: ({ color, size }) => (
-        <AntDesign name="home" color={color} size={size} />
-      ),
-    }} name="Home" children={() => <HomeStack isSetDatabaseInitialized={isSetDatabaseInitialized}/>} />
-      <Tab.Screen options={{
-      tabBarLabel: 'Help',
-      tabBarIcon: ({ color, size }) => (
-        <AntDesign name="questioncircleo" color={color} size={size} />
-      ),
-    }} name="Help" component={Help} />
-      <Tab.Screen options={{
-      tabBarLabel: 'Stats',
-      tabBarIcon: ({ color, size }) => (
-        <AntDesign name="barschart" color={color} size={size} />
-      ),
-    }} name="Stats" component={Stats} />
-      <Tab.Screen options={{
-      tabBarLabel: 'Settings',
-      tabBarIcon: ({ color, size }) => (
-        <AntDesign name="setting" color={color} size={size} />
-      ),
-    }} name="Settings" component={Settings} />
-    </Tab.Navigator>
-    </SafeAreaView>
+      <SafeAreaView
+        style={{ flex: 1, paddingTop: Platform.OS === "android" ? 35 : 0 }}
+      >
+        <Tab.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Tab.Screen
+            options={{
+              tabBarLabel: "Home",
+              tabBarIcon: ({ color, size }) => (
+                <AntDesign name="home" color={color} size={size} />
+              ),
+            }}
+            name="Home"
+            children={() => (
+              <HomeStack isSetDatabaseInitialized={isSetDatabaseInitialized} />
+            )}
+          />
+          <Tab.Screen
+            options={{
+              tabBarLabel: "Help",
+              tabBarIcon: ({ color, size }) => (
+                <AntDesign name="questioncircleo" color={color} size={size} />
+              ),
+            }}
+            name="Help"
+            component={Help}
+          />
+          <Tab.Screen
+            options={{
+              tabBarLabel: "Stats",
+              tabBarIcon: ({ color, size }) => (
+                <AntDesign name="barschart" color={color} size={size} />
+              ),
+            }}
+            name="Stats"
+            component={Stats}
+          />
+          <Tab.Screen
+            options={{
+              tabBarLabel: "Settings",
+              tabBarIcon: ({ color, size }) => (
+                <AntDesign name="setting" color={color} size={size} />
+              ),
+            }}
+            name="Settings"
+            component={Settings}
+          />
+        </Tab.Navigator>
+      </SafeAreaView>
     </NavigationContainer>
-  )
+  );
 }
